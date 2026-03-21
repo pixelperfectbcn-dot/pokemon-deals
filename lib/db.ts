f618442500d92b8097a677facce8a24d445062de
pixelperfectbcn-dot <pixelperfectbcn@gmail.com>
2026-03-22 00:32:48 +0100

    update db

--- lib/db.ts
+++ lib/db.ts
@@ -5,13 +5,26 @@ declare global {
   var __pokemonDealsPool: Pool | undefined;
 }
 
-function getPool() {
+function createPool() {
   const connectionString = process.env.DATABASE_URL;
-  if (!connectionString) throw new Error("DATABASE_URL no está configurada");
+  if (!connectionString) {
+    throw new Error("DATABASE_URL no está configurada");
+  }
+
   if (!global.__pokemonDealsPool) {
     global.__pokemonDealsPool = new Pool({ connectionString });
   }
+
   return global.__pokemonDealsPool;
 }
 
-export const pool = getPool();
+export function getPool() {
+  return createPool();
+}
+
+// Wrapper lazy para evitar que Next intente conectar a la DB durante el build
+export const pool = {
+  query: async (...args: Parameters<Pool["query"]>) => {
+    return createPool().query(...args);
+  }
+};
